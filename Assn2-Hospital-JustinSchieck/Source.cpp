3cc3ddd049691d948432f13d52bb6085c4197612
Justin Schieck <schieck91@gmail.com>
2017-06-05 18:39:16 -0400

    Second Commit

--- Assn2-Hospital-JustinSchieck/Source.cpp
+++ Assn2-Hospital-JustinSchieck/Source.cpp
@@ -1,15 +1,27 @@
 #include <iostream>
 using namespace std;
-int get_rate_drop_factor(int,int);
+
+
+//Justin Schieck
+//200328630
+//Might put some serious validation in if bored.
+
+
+//variables
 int menuChoice, calc;
+
+//functions initialization
 int get_problem();
+int get_rate_drop_factor();
+int get_kg_rate_conc();
+int get_unit_conc();
+int fig_drops_min(int, int);
 int fig_ml_hr(int);
 double by_weight(float, int, int);
 int by_units(int, int);
-//Justin Schieck
-//200328630
 
 
+//functions code
 int get_problem() {
 	int choice;
 
@@ -24,11 +36,62 @@ int get_problem() {
 	return choice;
 }
 
-int get_rate_drop_factor(int rate, int dropRate) {
+int get_rate_drop_factor() {
 
-	int dropsMin;
-	dropsMin = (rate*dropRate) / 60;
-	return dropsMin;
+	int rate, dropRate, dropPerMin;
+
+	cout << "Enter rate in ml / hr " << endl;
+	cin >> rate;
+	cout << "Enter tubings drop factor in drops / ml:  " << endl;
+	cin >> dropRate;
+	dropPerMin = fig_drops_min(rate, dropRate);
+	
+	return dropPerMin;
+
+}
+
+
+int get_kg_rate_conc() {
+	//Prompts the user to enter the data required for problem 3, 
+	//and sends this data back to the calling module via output parameters
+	float rateMg;
+	int concentrationMg, weight;
+	double totalMl;
+
+	cout << "Enter rate in mg / kg / hr: " << endl;
+	cin >> rateMg;
+	cout << "Enter patient weight in kg : " << endl;
+	cin >> weight;
+	cout << "Enter concentration in mg / ml : " << endl;
+	cin >> concentrationMg;
+	
+
+	totalMl = by_weight(rateMg, weight, concentrationMg);
+	return totalMl;
+}
+
+int get_unit_conc() {
+	//Prompts the user to enter the data required for problem 4, 
+	//and sends this data back to calling module via output parameters
+	int unitHour, conUnit, returnUnit;
+	cout << "Enter rate in units / hr: " << endl;
+	cin >> unitHour;
+	cout << "Enter concentration in units / ml : " << endl;
+	cin >> conUnit;
+
+	returnUnit = by_units(unitHour, conUnit);
+	return returnUnit;
+}
+
+int fig_drops_min(int rateMgKg,int dropFactor ) {
+	// Takes rate and drop factor as input parameters and returns drops/min 
+	//(rounded to nearest whole drop) as a function value
+	int returnAmount;
+	
+	returnAmount = (rateMgKg * dropFactor) / 60;
+
+	return returnAmount;
+
 
 }
 
@@ -56,71 +119,59 @@ int by_units(int unitHour, int conUnit) {
 
 int main()
 {
-	menuChoice = get_problem();
-	switch (menuChoice) {
-	case(1): {
-
-		int rate, dropRate;
-
-		cout << "Enter rate in ml / hr " << endl;
-		cin >> rate;
-		cout << "Enter tubings drop factor in drops / ml:  " << endl;
-		cin >> dropRate;
-		calc = get_rate_drop_factor(rate,dropRate);
-		cout << "The drop rate per minute is: " << calc << endl;
-		system("pause");
-		return(0);
-	}
-	case(2): {
+	//runs program until 5 is pressed.
+	do {
 		
-		int hours, lPerHour;
+		menuChoice = get_problem();
+		
+		switch (menuChoice) {
+		case(1): {
 
-		cout << "Enter number of hours: " << endl;
-		cin >> hours;
-		lPerHour = fig_ml_hr(hours);
-		cout << "The rate in millilitres per hour is: " << lPerHour << endl;
-		system("pause");
-		return(0);
+			int calc;
 
-	}
-	case(3): {
+			calc = get_rate_drop_factor();
 
-		int weightKg, conMg;
-		float rateMeasure;
-		double totalMl;
-		cout << "Enter rate in mg / kg / hr: " << endl;
-		cin >> rateMeasure;
-		cout << "Enter patient weight in kg : " << endl;
-		cin >> weightKg;
-		cout << "Enter concentration in mg / ml : " << endl;
-		cin >> conMg;
-		totalMl = by_weight(rateMeasure, weightKg, conMg);
-
-		cout << "The rate in millilitres per hour is: " << totalMl << endl;
-		system("pause");
-		return(0); 
-
-	}
-	case(4): {
-		int unitHour, conUnit, totalUnit;
-		cout << "Enter rate in units / hr: " << endl;
-		cin >> unitHour;
-		cout << "Enter concentration in units / ml : " << endl;
-		cin >> conUnit;
-		totalUnit = by_units(unitHour, conUnit);
-
-		cout << "The rate in millilitres per hour is: " << totalUnit << endl;
-		system("pause");
-		return(0);
-
-	}
-	case(5): {
-		cout << "Thank you for running this Program..." << endl;
-		system("pause");
-		return(0);
+			cout << "The drop rate per minute is: " << calc << endl;
+			break;
 		}
-	}
+		case(2): {
 
+			int hours, lPerHour;
+
+			cout << "Enter number of hours: " << endl;
+			cin >> hours;
+			lPerHour = fig_ml_hr(hours);
+			cout << "The rate in millilitres per hour is: " << lPerHour << endl << endl;
+			break;
+
+		}
+		case(3): {
+
+			double rateMeasure;
+
+			rateMeasure = get_kg_rate_conc();
+
+			cout << "The rate in millilitres per hour is: " << rateMeasure << endl << endl;
+
+			break;
+		}
+		case(4): {
+			int totalUnit;
+			totalUnit = get_unit_conc();
+
+			cout << "The rate in millilitres per hour is: " << totalUnit << endl << endl;
+
+			break;
+		}
+
+		case(5): {
+			cout << "Thank you for running this Program..." << endl << endl;
+
+			}
+		}
+	} while (menuChoice != 5);
+	system("pause");
+	return(0);
 }
 	
 
